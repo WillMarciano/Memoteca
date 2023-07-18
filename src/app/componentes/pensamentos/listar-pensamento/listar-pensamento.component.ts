dc2d2fba73a109eab16ba45310b2fa71577fc14c
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-07-18 16:17:41 -0300

    Mostrando a lista de favoritos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -37,4 +37,12 @@ export class ListarPensamentoComponent implements OnInit {
       this.listaPensamentos = listaPensamentos
     })
   }
+
+  listarFavoritos(){
+    this.existeMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service.listarPensamentosFavoritos(this.paginaAtual, this.filtro).subscribe(lista => {
+      this.listaPensamentos = lista
+    })
+  }
 }
