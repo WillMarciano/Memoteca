1e261179b67f487aec3fbded93c75c78b9aefcae
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2023-07-18 00:07:35 -0300

    Botão para carregar mais dados

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -11,11 +11,21 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1
+  existeMaisPensamentos: boolean = true;
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
+    })
+  }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length){
+        this.existeMaisPensamentos = false
+      }
     })
   }
 
